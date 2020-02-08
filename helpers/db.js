390b9414d353bb27efff9587ecfad23c73a136dd
Mehmet VAROL <mehmt.varol@gmail.com>
2020-02-08 23:59:53 +0300

    config edit

--- helpers/db.js
+++ helpers/db.js
@@ -1,14 +1,17 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose')
 
 module.exports = () => {
-    mongoose.connect("mongodb+srv://root:12541430@cluster0-rnwwk.mongodb.net/todo",  {useNewUrlParser: true });
+  mongoose.connect(
+    'mongodb+srv://root:12541430@cluster0-rnwwk.mongodb.net/todo',
+    { useUnifiedTopology: true, useNewUrlParser: true }
+  )
 
-    mongoose.connection.on('open', () => {
-        console.log('MongoDB: Connected');
-      });
-      mongoose.connection.on('error', (err) => {
-        console.log('MongoDB: Error', err);
-      });
+  mongoose.connection.on('open', () => {
+    console.log('MongoDB: Connected')
+  })
+  mongoose.connection.on('error', err => {
+    console.log('MongoDB: Error', err)
+  })
 
-    mongoose.Promise = global.Promise;
+  mongoose.Promise = global.Promise
 }
